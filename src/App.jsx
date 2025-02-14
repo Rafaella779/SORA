92d9ac15a1a9af1adc2487461f3fdd5c7d28d52a
rafaella <rafaellasabenorio159@gmail.com>
2025-02-14 11:51:57 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,10 @@ function App() {
         <Route path="RegisterS" element={<RegisterS/>} />
         <Route path="UserDashboard" element={<UserDashboard/>} />
         <Route path="RegisterT" element={<RegisterT/>} />
+<<<<<<< HEAD
+=======
+        <Route path="AboutUs" element={<AboutUs/>} />
+>>>>>>> 5d24bd0172703e86085321d456ebb7be192c79df
       </Route>
     </Routes>
     </>
