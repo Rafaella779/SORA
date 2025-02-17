28a4051258fc028af14c8c597098197920eec106
rafaella <rafaellasabenorio159@gmail.com>
2025-02-17 14:19:50 +0800

    UserDashboard-Rafaella

--- src/Pages/UserDashboard.jsx
+++ src/Pages/UserDashboard.jsx
@@ -3,19 +3,17 @@ import React from "react";
 import {Form, Container, Nav, Navbar, NavDropdown, Button,} from 'react-bootstrap'
 
 export default function UserDashboard() {
-
+	
 
 	return(
 
 		<div>
 
-		<div className="cinzel-decorative d-flex align-item-begin">
-			    	<h1>SORA</h1>
-			    </div>
-			<div expand="lg" >
-				<Navbar className="border-3px">
+		<div className="border-3px">
+			<Navbar>
 			      <Container fluid>
 			        <Navbar.Toggle aria-controls="navbarScroll" />
+			        <Navbar.Brand href="#" className="cinzel-decorative">SORA</Navbar.Brand>
 			        <Navbar.Collapse id="navbarScroll">
 			          <Nav
 			            className="me-auto my-2 my-lg-0"
@@ -44,36 +42,37 @@ export default function UserDashboard() {
           			</Form>
 			    </Navbar>
 			</div> 
-
-				 <div className="container border-3px"></div>
-				<div className="col-1">
-
-					<Nav className="border-3px">
-						<Nav.Link href="#action1">User</Nav.Link>
-						<Nav.Link href="#action2">SchoolName</Nav.Link>
-						<Nav.Link href="#action3">School ID</Nav.Link>
-						<Nav.Link href="#action4">ID</Nav.Link>
-						<Nav.Link href="#action5">Birthdate</Nav.Link>
-						<Nav.Link href="#action6">LRN</Nav.Link>
-						<Nav.Link href="#action7">Email</Nav.Link>
-						<Nav.Link href="#action8">Papers</Nav.Link>
-						<h3>__________</h3>
-						<Nav.Link href="#action9">About us</Nav.Link>
-						<Nav.Link href="#action10">Contact us</Nav.Link>
-					</Nav>
-				</div>
-				<div>
-					<h3  className="d-flex justify-content-center align-items-center">Welcome User</h3>
-					<p className="d-flex justify-content-center align-items-center">SORA</p>
-				</div>
-
 				<div className="d-flex">
 					<div className="border-3px col">
-						<Form className="w-500"></Form>
+						<h1>Hello Admin!</h1>
+							<div className="d-flex gap-5">
+								<Nav>
+									<Nav.Link href="#action1">User</Nav.Link>
+									<Nav.Link href="#action2">SchoolName</Nav.Link>
+									<Nav.Link href="#action3">School ID</Nav.Link>
+									<Nav.Link href="#action4">ID</Nav.Link>
+									<Nav.Link href="#action5">Birthdate</Nav.Link>
+									<Nav.Link href="#action6">LRN</Nav.Link>
+									<Nav.Link href="#action7">Email</Nav.Link>
+									<Nav.Link href="#action8">Papers</Nav.Link>
+									<h3>__________</h3>
+									<Nav.Link href="#action9">About us</Nav.Link>
+									<Nav.Link href="#action10">Contact us</Nav.Link>
+								</Nav>
+							</div>
+							<div className="border-3px d-flex">
+								<div>
+									<Form className="w-600">
+										<h1>Hatdog</h1>
+										<p>
+											jafuhdyksgfukydsvgb8sirtguv
+										</p>
+									</Form>
+									
+								</div>
+							</div>
 					</div>
-				</div>
-
-		</div>
+			</div>
 	)
 }
 
